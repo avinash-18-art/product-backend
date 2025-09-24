a3fa996b6537f677a6d3ba119d9053114c3ef759
avinash-18-art <your-email@example.com>
2025-09-24 10:18:09 +0530

    add the new index

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const JWT_SECRET = "apjabdulkalam@545";
 
+app.use(cors({
+  origin:"https://meesho-frontend-no7l.vercel.app",
+  methods:['GET','POST','PUT','DELETE'],
+  credentials:true, // If you need to send cookies or authentication headers with the request
+})
+);
+
 // ===== Mongoose Connection =====
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -29,12 +36,7 @@ mongoose.connect(process.env.MONGO_URI, {
 const User = require("./models/User");
 
 // ===== Middleware =====
-app.use(cors({
-  origin:"https://meesho-frontend-no7l.vercel.app",
-  methods:['GET','POST','PUT','DELETE'],
-  credentials:true, // If you need to send cookies or authentication headers with the request
-})
-);
+
 
 app.use(express.json());
 const upload = multer({ dest: "uploads/" });
